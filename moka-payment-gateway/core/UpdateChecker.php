408d046c2ef5504b4aca89276e3149c1baa578fb
Fatih Toprak <patron@fatihtoprak.com>
2021-12-07 22:59:13 +0300

    Added MocaPay Core class, and update checker class

---
+++ moka-payment-gateway/core/UpdateChecker.php
@@ -0,0 +1,21 @@
+<?php 
+
+declare(strict_types=1);
+
+class OptimisthubUpdateChecker 
+{
+    public $endpoint = 'https://moka.wooxup.com/check';
+    public $platform = 'wordpress';
+    public $currentVersion = '2.0';
+
+    public function __invoke()
+    {
+
+    }
+
+    private function doRequest()
+    {
+
+    }
+
+}
