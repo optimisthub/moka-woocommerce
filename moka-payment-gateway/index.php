31e404d609f341f6e729e1c8c1993079e2d33471
Fatih Toprak <patron@fatihtoprak.com>
2021-12-07 20:11:46 +0300

    Initialize new plugin index.

---
+++ moka-payment-gateway/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: Moka Payment Gateway for WooCommerce
+ * Plugin URI: https://www.kahvedigital.com
+ * Description: Moka Payment gateway for woocommerce
+ * Version: 1.0.0
+ * Author: Optimist Hub
+ * Author URI: https://optimisthub.com?ref=mokaPayment
+ * Domain Path: /i18n/languages/
+ */
