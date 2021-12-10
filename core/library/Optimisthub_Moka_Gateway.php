ca0778c3a4e0f6886fe1ed341262832d22826471
Fatih Toprak <patron@fatihtoprak.com>
2021-12-10 16:54:11 +0300

    payment fields overwrited

--- core/library/Optimisthub_Moka_Gateway.php
+++ core/library/Optimisthub_Moka_Gateway.php
@@ -28,15 +28,15 @@ function initOptimisthubGatewayClass()
             $this->description = $this->get_option( 'description' );
             $this->enabled = $this->get_option( 'enabled' );
             $this->testmode = 'yes' === $this->get_option( 'testmode' );
+            $this->installment = 'yes' === $this->get_option( 'installment' );
             $this->company_code = $this->get_option( 'company_code' );
             $this->company_name = $this->get_option( 'company_name' );
             $this->api_username = $this->get_option( 'api_username' );
             $this->api_password = $this->get_option( 'api_password' );
-                
-    
     
             add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, [ $this, 'process_admin_options' ] ); 
             add_action( 'wp_enqueue_scripts', [ $this, 'payment_scripts' ] ); 
+            add_filter( 'woocommerce_credit_card_form_fields' , [$this,'payment_form_fields'] , 10, 2 );
     
             
         }
@@ -74,6 +74,12 @@ function initOptimisthubGatewayClass()
                     'default'     => 'yes',
                     'desc_tip'    => true,
                 ],
+                'installment' => [
+                    'title'       => __( 'Installement', 'moka-woocommerce' ),
+                    'label'       => __('Enable/Disable Installement ?', 'moka-woocommerce' ),
+                    'type'        => 'checkbox', 
+                    'default'     => 'yes',
+                ],
                 'company_code' => [
                     'title'       => __( 'Company Code', 'moka-woocommerce' ),
                     'type'        => 'text', 
@@ -92,30 +98,89 @@ function initOptimisthubGatewayClass()
                 ],
             ];		
         }
+
+
+        
+        public function payment_form_fields($cc_fields , $payment_id){
+ 
+            $cc_fields = [
+                'name-on-card' => '
+                    <p class="form-row form-row-wide">
+                        <label for="'. $payment_id.'-card-holder">' . __('Name On Card','moka-woocommerce') . ' <span class="required">*</span></label>
+                        
+                        <input 
+                            id="'.$payment_id.'-card-holder" 
+                            class="input-text wc-credit-card-form-card-holder" 
+                            type="text" 
+                            autocomplete="off" 
+                            placeholder="' . __('Name On Card','moka-woocommerce') . '" 
+                            name="' .$payment_id. '-name-oncard" 
+                        />
+                    </p>',
+                'card-number-field' => '
+                    <p class="form-row form-row-wide">
+                        <label for="'.$payment_id.'-card-number">' . __( 'Card Number', 'moka-woocommerce' ) . ' <span class="required">*</span></label>
+                        
+                        <input 
+                            id="'.$payment_id.'-card-number" 
+                            class="input-text wc-credit-card-form-card-number" 
+                            type="text" 
+                            maxlength="20" 
+                            autocomplete="off" 
+                            placeholder="•••• •••• •••• ••••" 
+                            name="'.$payment_id . '-card-number"
+                        />
+                    </p>',
+                'card-expiry-field' => '
+                    <p class="form-row form-row-first">
+                        <label for="'.$payment_id.'-card-expiry">' . __( 'Expiry (MM/YY)', 'woocommerce' ) . ' <span class="required">*</span></label>
+                
+                        <input 
+                            id="'.$payment_id.'-card-expiry" 
+                            class="input-text wc-credit-card-form-card-expiry" 
+                            type="text" 
+                            autocomplete="off" 
+                            placeholder="' . __( 'MM / YY', 'woocommerce' ) . '" 
+                            name="'.$payment_id.'-card-expiry"
+                        />
+                </p>',
+                'card-cvc-field' => '
+                    <p class="form-row form-row-last">
+                        <label for="'.$payment_id.'-card-cvc">' . __( 'Card Code', 'moka-woocommerce' ) . ' <span class="required">*</span></label>
+                        
+                        <input 
+                            id="'.$payment_id.'-card-cvc" 
+                            class="input-text wc-credit-card-form-card-cvc" 
+                            type="text" 
+                            autocomplete="off" 
+                            placeholder="' . __( 'CVC', 'woocommerce' ) . '" 
+                            name="'.$payment_id.'-card-cvc" 
+                        />
+                </p>'
+            ];
+            return $cc_fields;
+        }
+
     
         public function payment_fields() 
         {
     
             if ( $this->description ) { 
                 if ( $this->testmode ) {
-                    $this->description .= ' TEST MODE ENABLED. In test mode, you can use the card numbers listed in <a href="#">documentation</a>.';
+                    $this->description .=  __( "TEST MODE ENABLED. In test mode, you can use the card numbers listed in <a href='#''>documentation</a>", 'moka-woocommerce' );
                     $this->description  = trim( $this->description );
                 } 
                 echo wpautop( wp_kses_post( $this->description ) );
             } 
-            
-            echo '<div class="card-js">';
- 
- 
+             
             do_action( 'woocommerce_credit_card_form_start', $this->id );
             
             $cc_form           = new WC_Payment_Gateway_CC();
             $cc_form->id       = $this->id;
-            $cc_form->supports = $this->supports;
+            $cc_form->supports = $this->supports; 
             $cc_form->form();
         
-            do_action( 'woocommerce_credit_card_form_end', $this->id ); 
-            echo '</div>'; 
+            do_action( 'woocommerce_credit_card_form_end', $this->id );  
         }
  
         public function payment_scripts() 
