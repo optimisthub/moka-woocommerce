50f07ca702c7a106df1bfe053d9f5af815a11492
Fatih Toprak <patron@fatihtoprak.com>
2022-09-30 18:47:16 +0300

    version 3.0.1 released.

--- core/library/Moka_Init.php
+++ core/library/Moka_Init.php
@@ -3,7 +3,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define('OPTIMISTHUB_MOKA_PAY_VERSION', '3.0');
+define('OPTIMISTHUB_MOKA_PAY_VERSION', '3.0.1');
 
 global $mokaVersion;
 $mokaVersion = OPTIMISTHUB_MOKA_PAY_VERSION;
